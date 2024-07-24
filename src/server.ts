15f2b00f9aff58b63170eda442db61d48157eca4
sarwar-asik <sarwarasik@gmail.com>
2024-07-24 19:01:31 +0600

    v2-optimize: optized error of server @error

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,33 @@
+import "colors";
 import { Server } from 'http';
 import app from './app';
 import config from './config';
-import "colors"
+import { errorlogger } from "./shared/logger";
 
 
 async function main() {
 
   const server: Server = app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`.green.underline.bold);
+    console.log(`Server running on port http://localhost:${config.port}`.green.underline.bold);
   });
 
   const exitHandler = () => {
 
     if (server) {
       server.close(() => {
-        console.log('Server closed');
+        config.env === 'production'
+          ? errorlogger.error(error)
+          : console.log(error);
+        process.exit(1);
       });
     }
     process.exit(1);
   };
 
   const unexpectedErrorHandler = (error: unknown) => {
-    console.log(error);
+    config.env === 'production'
+    ? errorlogger.error(error)
+    : console.log('unexpectedErrorHandler is detected ......', error);
     exitHandler();
   };
 
